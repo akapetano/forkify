7445e94931671b946ec8b290db2a236f8d6c1364
Andrija Kapetanović <andrija.kapetanovic@gmail.com>
2022-01-31 14:53:13 +0100

    Added the complete project.

---
+++ forkify/src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'ac0f9856-27dc-4b50-9ebf-57142e98a6e1';
+export const MODAL_CLOSE_SEC = 2.5;
